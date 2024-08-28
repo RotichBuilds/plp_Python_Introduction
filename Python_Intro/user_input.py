3a121c6a64687effb8d2694c0b6e98c4d60ca441
RotichBuilds <134204818+RotichBuilds@users.noreply.github.com>
2024-08-28 11:55:44 +0300

    Add files via upload

---
+++ Python_Intro/user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name?: ")
+age = input("Please enter your age: ")
+location = input("What is your location? ")
+
+print(name, "are you", age, " years old from", location, "?")
